7cdbe990ba8676bc566c3102aab445419d6d0219
A.Shaing <a09281234@gmail.com>
2018-01-18 16:40:48 +0800

    add no.19

---
+++ 19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int x = 15;
+int y = 3;
+
+int main(int argc, char* argv[])
+{
+    x = x ^ y;
+    cout << x << endl;
+    y = x ^ y;
+    cout << y << endl;
+    x = x ^ y;
+    cout << x << endl;
+    cout << (y >> x) << endl;
+    // cout == 3
+    return 0;
+}
