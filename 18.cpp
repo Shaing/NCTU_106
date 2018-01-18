7b255ad69d1aeec15e275319d4d35de6ea5bbef4
A.Shaing <a09281234@gmail.com>
2018-01-18 18:09:50 +0800

    add no.18

---
+++ 18.cpp
@@ -0,0 +1,28 @@
+#include <iostream>  
+#include <bitset>  
+using namespace std;  
+int a[10] = {8, 15, 23, 30, 7, 2, 10, 13, 6, 5};
+  
+int main() 
+{
+    int k;
+    for(int i = 0; i < 10; i++)
+    {
+        k = i;
+        for(int j = i + 1; j < 10; j++)
+        {
+            if(a[j] > a[k])
+                k = j;
+            if(k != i)
+            {
+                int m;
+                m = a[i];
+                a[i] = a[k];
+                a[k] = m;
+            }
+        }
+    }  
+    cout << a[0] << endl;
+    //cout == 30
+    return 0;
+} 
